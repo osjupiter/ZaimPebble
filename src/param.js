d4c34015e6d043af4bbfddd69b40b0c66eda3b52
osjupiter <osjupiter@users.noreply.github.com>
2015-05-25 10:37:37 +0900

    edit param

--- src/param.js
+++ src/param.js
@@ -1,5 +1,5 @@
 var param = module.exports;
 param.consumer={
-  Secret: "1ae62c05da6ca0ededf1293b144c9b839bbd79b6",
-  Key:"86c38f98031f0c5ff79a77e791d1229d876cfdd6"
+  Secret: "your secret",
+  Key:"your key"
 };
